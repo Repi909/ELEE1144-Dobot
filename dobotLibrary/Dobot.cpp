ce496f819653a066c8d7ece6d0a92314c6b07d18
th5528e <th5528e@gre.ac.uk>
2024-01-11 09:40:52 +0000

    Adding current dobot files

--- dobotLibrary/Dobot.cpp
+++ dobotLibrary/Dobot.cpp
@@ -1,4 +1,4 @@
-#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\cores\arduino\Arduino.h"
+#include "Arduino.h"
 #include "Dobot.h"
 #include "SoftwareSerial.h"
 
@@ -8,7 +8,6 @@ byte  getPose[] = {170,170,2,10,0,246};
 byte incomingByte; //to collect the response from Dobot
 byte message[100];  //longer than the longest possible message
 int i;
-
 //Union type makes converting from byte array to float easy:
 union coord {
   byte cByte[4];
@@ -16,28 +15,30 @@ union coord {
 };
 coord cart[4];  //position of the arm in cartesian coordinates
 coord pol[4];  //position of the arm in polar coordinates
+///////////////////////////////////////////////////////////////
 
-
-// Empty Constructor
+///////////////////////////  Empty Constructor  ///////////////
 Dobot::Dobot(){
 };
+///////////////////////////////////////////////////////////////
 
+////////////////////////////// Setup //////////////////////////
 void Dobot::begin(){
     Serial.begin(115200);
     pinMode(2,INPUT);
     pinMode(3,OUTPUT);
     mySerial.begin(115200);  //Dobot baud rate
 }
-//  Print Current Position
-void Dobot::printPose(){
+
+
+////////////////////////////////////  Print Current Pose  ///////////
+void  Dobot::printPose(){
 
 for(int i=0;i<6;i++){
   mySerial.write(getPose[i]);
 }
 
-//allow time for the Dobot to serial send a response
-delay(50);
-
+delay(50);  //allow time for the Dobot to serial send a response
 //Read response back from Dobot:
 i=0;
 while (mySerial.available() > 0){
@@ -56,7 +57,6 @@ for(i=0;i<=3;i++){
   pol[2].cByte[i] = message[i+29];
   pol[3].cByte[i] = message[i+33];
 }
-
 Serial.print("Cartesian: ");
 Serial.print(cart[0].cFloat);  //The union type means the float equivalent is available
 Serial.print(" ");
@@ -73,22 +73,33 @@ Serial.print(" ");
 Serial.print(pol[2].cFloat);
 Serial.print(" ");
 Serial.println(pol[3].cFloat);
+
+Serial.println("Full dobot message: ");
+for(int i=0; i<38; i++){
+Serial.print(message[i]);
+Serial.print(' ');
+}
+Serial.println();
 delay(1000);
 }
+//////////////////////////////////////////////////////////////////////////////
+
+
+
 
 void Dobot::commandFrame(byte comFrame[]){
-
-//Start Joint Movement
-
-for(int i=0;i<8;i++){
+  
+for(int i=0;i<(comFrame[2]+4);i++){   //The third comFrame byte gives the number of payload bytes; total number of bytes is 4 more than this
   mySerial.write(comFrame[i]);
 }
 delay(50);
 
-//read response and ignore it
+//read response and send it to the serial monitor:
+Serial.print ("Dobot response:  ");
 while (mySerial.available() > 0){
-  message[i] = mySerial.read();
-  i++;
-
-}
+  message[0] = mySerial.read();
+  Serial.print(message[0]);
+  Serial.print(' ');
+  }
+  Serial.println();
 }
--- dobotLibrary/Dobot.h
+++ dobotLibrary/Dobot.h
@@ -1,5 +1,6 @@
-#ifndef Dobot_h
-#define Dobot_h
+#pragma once
+#ifndef DOBOT_H
+#define DOBOT_H
 
 #include "Arduino.h"
 class Dobot
@@ -9,6 +10,12 @@ class Dobot
         void printPose();
         void begin();
         void commandFrame(byte comFrame[]);
+        void jogXp(); //not yet implemented
+        void jogXm(); //not yet implemented
+        void jogYp(); //not yet implemented
+        void jogYm(); //not yet implemented
+        void jogZp(); //not yet implemented
+        void jogZm(); //not yet implemented
 };
 
 #endif
