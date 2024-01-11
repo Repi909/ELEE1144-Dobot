b07fd58e299059ad16ab9b93d378b86237055e4f
Jake Willshire <jake.willshire@matthey.com>
2024-01-11 11:28:43 +0000

    Map library with set positions for each block. The function will be inherited by main.cpp so code is clean and saves re-printing of byte array.

---
+++ dobotLibrary/blockMap.cpp
@@ -0,0 +1,17 @@
+#include "blockMap.h"
+#include <vector>
+#include <map>
+
+std::vector<std::vector<uint8_t>> bMap = {
+                {170,170,19,84,3,0,0,0,52,67,0,0,0,0,0,0,0,0,0,0,0,50}, //home
+                {170,170,19,84,3,0,0,0,0x48,0x43,0,0,0x70,0xc3,0,0,0x5c,0xc2,0,0,0,0,205},  //pickup
+            };
+
+    blockMap::blockMap(){
+        bMap;
+    };
+
+    std::vector<uint8_t> accessValue(uint8_t blockType, uint8_t counterValue){
+            
+        return bMap.at(blockType + counterValue);
+    };
